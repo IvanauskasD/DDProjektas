9f8810de115f01b99c69225f662cb19c152cfd08
Domantas Ivanauskas <ivanauskas.dom@gmail.com>
2018-03-10 21:16:52 +0200

    Sukurtas pradinis puslapis, pradetas kurti prisijungimo funkcionalumas

---
+++ templates/Student.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: domantas
+ * Date: 18.3.10
+ * Time: 20.34
+ */
+
+class Student
+{
+
+}
---
+++ templates/first.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: domantas
+ * Date: 18.3.10
+ * Time: 21.04
+ */
