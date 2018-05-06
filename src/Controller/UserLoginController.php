1fd0fb60dba5db6f3393df12edfdded217f86010
Domantas <ivanauskas.dom@gmail.com>
2018-05-06 17:40:11 +0300

    Company and User entities work as seperate entities

---
+++ src/Controller/UserLoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Events;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class UserLoginController extends Controller
+{
+    /**
+     * @Route("/loginUser", name="loginUser")
+     * @param AuthenticationUtils $authenticationUtils
+     * @param AuthorizationCheckerInterface $authChecker
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \LogicException
+     */
+    public function loginAction(AuthorizationCheckerInterface $authChecker, AuthenticationUtils $authenticationUtils)
+    {
+
+        if ($authChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        $errors = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUserName=  $authenticationUtils->getLastUsername();
+        if ($errors == true)
+        {
+            $errors = "Incorrect username and/or password";
+        }
+        return $this->render('Login/userLogin.html.twig', [
+            'errors'=>$errors,
+            '$lastUserName' =>$lastUserName
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+
+    }
+}
